d5c503f78cb5da2f1890e4389473ae6f2f5c215d
Adham <adhammahgoub1@gmail.com>
2026-04-02 20:39:40 +0200

    some cleaning

--- NN_Trading_project/helpers/real_life_test/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Helpers for real-life inference/scanning workflows."""
-
-from .start_of_day_scan import build_model_features_from_df, run_start_of_day_scan
-
-__all__ = ["build_model_features_from_df", "run_start_of_day_scan"]
